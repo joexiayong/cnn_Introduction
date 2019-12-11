b1fd761c9dbf7170b98bf56eb0e4908d4ab4ca2c
Xia Yong <joexiayong@foxmail.com>
2019-12-11 23:10:59 +0800

    use gpu on windows

--- cnn/mytrain.py
+++ cnn/mytrain.py
@@ -3,9 +3,12 @@ import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 import datetime
 
+
+# os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+
+
 class DataSource(object):
     def __init__(self):
-
         data_path = os.path.abspath(os.path.dirname(__file__))
         data_path += '/../mnist.npz'
         mnist = datasets.mnist
@@ -27,8 +30,30 @@ class DataSource(object):
         self.test_labels = test_labels
 
 
+class UseGpu(object):
+    def __init__(self):
+        self.gpu_memory = 570
+        self.gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
+
+    def this_device_use_gpu(self):
+
+        tf.config.experimental.set_memory_growth(self.gpus[0], True)
+        tf.config.experimental.set_virtual_device_configuration(self.gpus[0],
+                                                                [tf.config.experimental.VirtualDeviceConfiguration(
+                                                                    memory_limit=self.gpu_memory)])
+
+    def is_device_use_gpu(self):
+        if tf.test.is_gpu_available():
+            self.this_device_use_gpu()
+        else:
+            os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
 class CNN(object):
     def __init__(self):
+
+        UseGpu().is_device_use_gpu()
+
         model = models.Sequential()
         # 第1层卷积，卷积核大小为3*3，32个，28*28为待训练图片的大小
         model.add(layers.Conv2D(
@@ -44,7 +69,6 @@ class CNN(object):
         model.add(layers.Dense(64, activation='relu'))
         model.add(layers.Dense(10, activation='softmax'))
 
-
         self.model = model
 
 
@@ -54,7 +78,7 @@ class Train:
         self.data = DataSource()
 
     def train(self):
-        check_path = './checkpoint/cp-{epoch:04d}.ckpt'
+        check_path = './point/cp-{epoch:04d}.ckpt'
         save_model_cb = tf.keras.callbacks.ModelCheckpoint(
             check_path, save_weights_only=True, verbose=1, period=5)
 
@@ -62,11 +86,11 @@ class Train:
                                loss='sparse_categorical_crossentropy',
                                metrics=['accuracy'])
 
-        log_dir="./logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        log_dir = "C:\\code\\cnn_Introduction\\logs\\" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
         tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
         self.cnn.model.fit(self.data.train_images, self.data.train_labels,
-                           epochs=5, callbacks=[save_model_cb,tensorboard_callback])
+                           epochs=5, callbacks=[save_model_cb, tensorboard_callback])
 
         test_loss, test_acc = self.cnn.model.evaluate(
             self.data.test_images, self.data.test_labels)
@@ -76,4 +100,3 @@ class Train:
 if __name__ == "__main__":
     app = Train()
     app.train()
-
--- cnn/predict.py
+++ cnn/predict.py
@@ -1,19 +1,24 @@
 import tensorflow as tf
 from PIL import Image
 import numpy as np
-
-from train import CNN
+import os
+from mytrain import CNN
 import warnings
 warnings.filterwarnings('ignore')
-
+# os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
+tf.config.experimental.set_virtual_device_configuration(gpus[0],
+[tf.config.experimental.VirtualDeviceConfiguration(memory_limit=100)])
 
 if __name__ == "__main__":
-    path = '/root/hand/image/888.png'
+    path = os.getcwd()
+    path += '\\..\\image\\888.png'
     img = Image.open(path).convert('L')
     flatten_img = np.reshape(img, (28, 28, 1))
     x = np.array([1 - flatten_img])
     cnn = CNN()
-    cnn.model.load_weights(tf.train.latest_checkpoint('./checkpoint'))
+    cnn.model.load_weights(tf.train.latest_checkpoint('/point'))
     predicted = cnn.model.predict(x)
 
     print(path)
